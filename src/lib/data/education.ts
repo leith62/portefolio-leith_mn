3107d01ced21892df7ac606ff24328bbd4e9d88a
leith62 <manaaleith62@gmail.com>
2024-11-20 23:33:09 +0100

    add experiance an education part

--- src/lib/data/education.ts
+++ src/lib/data/education.ts
@@ -1,31 +1,58 @@
 import Assets from './assets';
 import type { Education } from '../types';
 
-export const items: Array<Education> = [
+export const items: Array<Education> = 
+[
 	{
-		degree: 'Bachelor degree of Computer Science',
-		description: '',
-		location: 'Tunisia',
-		logo: Assets.Unknown,
-		name: '',
-		organization: 'ISTIC',
-		period: { from: new Date(2020, 0, 1), to: new Date(2022, 5, 1) },
-		shortDescription: '',
-		slug: 'dummy-education-item',
-		subjects: ['C', 'Algorithm', 'Algebra', 'Python', 'C++', 'Java', 'English']
+	  degree: 'Cycle d’Ingénieur en Informatique : BI & Big Data',
+	  description: '',
+	  location: 'Tunisia',
+	  logo: Assets.Unknown,
+	  name: '',
+	  organization: 'Iteam University',
+	  period: { from: new Date(2022, 0, 1) },
+	  shortDescription: '',
+	  slug: 'cycle-ingenieur-ibi-bigdata',
+	  subjects: ['Big Data', 'Business Intelligence', 'Data Science', 'Machine Learning', 'Algorithms', 'Database Systems']
 	},
 	{
-		degree: 'PhD of Computer Science',
-		description: '',
-		location: 'USA',
-		logo: Assets.Unknown,
-		name: '',
-		organization: 'MIT',
-		period: { from: new Date(2023, 0, 1) },
-		shortDescription: '',
-		slug: 'dummy-education-item-2',
-		subjects: ['Assembly', 'Rust', 'Computer Architecture', 'Algorithms and Data structures']
+	  degree: 'Licence en Mathématique et Informatique',
+	  description: '',
+	  location: 'Tunisia',
+	  logo: Assets.Unknown,
+	  name: '',
+	  organization: 'Faculté des Sciences de Tunis',
+	  period: { from: new Date(2019, 0, 1), to: new Date(2022, 5, 1) },
+	  shortDescription: '',
+	  slug: 'licence-math-info',
+	  subjects: ['Mathematics', 'Computer Science', 'Data Structures', 'Algorithms', 'Statistics', 'Programming']
+	},
+	{
+	  degree: 'Licence en Mathématique fondamentale',
+	  description: '',
+	  location: 'Tunisia',
+	  logo: Assets.Unknown,
+	  name: '',
+	  organization: 'Faculté des Sciences de Tunis',
+	  period: { from: new Date(2016, 0, 1), to: new Date(2018, 5, 1) },
+	  shortDescription: '',
+	  slug: 'licence-math-fondamentale',
+	  subjects: ['Pure Mathematics', 'Algebra', 'Calculus', 'Analysis', 'Geometry', 'Topology']
+	},
+	{
+	  degree: 'Cycle préparatoire Mathématique et Physique',
+	  description: '',
+	  location: 'Tunisia',
+	  logo: Assets.Unknown,
+	  name: '',
+	  organization: 'Faculté des Sciences de Tunis',
+	  period: { from: new Date(2012, 0, 1), to: new Date(2015, 5, 1) },
+	  shortDescription: '',
+	  slug: 'cycle-preparatoire-math-physique',
+	  subjects: ['Mathematics', 'Physics', 'Chemistry', 'General Science', 'Introduction to Programming']
 	}
-];
+  ]
+  
+;
 
 export const title = 'Education';
